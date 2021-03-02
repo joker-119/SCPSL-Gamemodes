7b15370cad316e1db148c24ad2170c6a860ce722
jorrit-erbium <jorrit@molenaarmail.nl>
2021-03-02 07:45:26 +0100

    Added Blackout gamemode (#10)
    * Added Blackout gamemode
    
    * Update AssemblyInfo.cs
    
    * Update AssemblyInfo.cs
    
    * Update README.md
    
    * Added descriptions
    
    * Update README.md
    
    * Update README.md

---
+++ Blackout/Methods.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Exiled.API.Features;
+using MEC;
+
+namespace Blackout
+{
+    public class Methods
+    {
+        private readonly Plugin plugin;
+        public Methods(Plugin plugin) => this.plugin = plugin;
+
+        private int GetScpCount => (plugin.Config.SmartScpSelection ? Player.List.Count() / 6 : plugin.Config.MaxScpCount) < 1 ? 1 : plugin.Config.SmartScpSelection ? Player.List.Count() / 6 : plugin.Config.MaxScpCount;
+
+        public void SetupPlayers()
+        {
+            Timing.CallDelayed(1f, () => SpawnScps());
+        }
+
+        void SpawnScps()
+        {
+           List<Player> players = Player.List.ToList();
+           for (int i = 0; i < GetScpCount; i++)
+           {
+                int r = plugin.Rng.Next(players.Count);
+                players[r].Role = plugin.Config.ScpToSpawn;
+                players.Remove(players[r]);
+            }
+        }
+        
+        internal void EventRegistration(bool disable = false)
+        {
+            switch (disable)
+            {
+                case true:
+                    Exiled.Events.Handlers.Server.RoundStarted -= plugin.EventHandlers.OnRoundStart;
+                    Exiled.Events.Handlers.Server.RoundEnded -= plugin.EventHandlers.OnRoundEnd;
+                    Exiled.Events.Handlers.Player.ChangingRole -= plugin.EventHandlers.OnChangingRole;
+                    break;
+                case false:
+                    Exiled.Events.Handlers.Server.RoundStarted += plugin.EventHandlers.OnRoundStart;
+                    Exiled.Events.Handlers.Server.RoundEnded += plugin.EventHandlers.OnRoundEnd;
+                    Exiled.Events.Handlers.Player.ChangingRole += plugin.EventHandlers.OnChangingRole;
+                    break;
+            }
+        }
+
+        public void EnableGamemode(bool force = false)
+        {
+            if (!force)
+                plugin.IsEnabled = true;
+            else
+            {
+                plugin.IsEnabled = true;
+                SetupPlayers();
+            }
+
+            plugin.ShouldDisableNextRound = true;
+        }
+
+        public void DisableGamemode(bool force = false)
+        {
+            if (!force)
+                plugin.ShouldDisableNextRound = true;
+            else
+            {
+                List<RoleType> scpRoles = new List<RoleType>
+                {
+                    RoleType.Scp049,
+                    RoleType.Scp079,
+                    RoleType.Scp096,
+                    RoleType.Scp106,
+                    RoleType.Scp173,
+                    RoleType.Scp93953,
+                    RoleType.Scp93989
+                };
+                
+                foreach (Player player in Player.List)
+                {
+                    if (player.Role == RoleType.Scp173)
+                    {
+                        player.Role = scpRoles[plugin.Rng.Next(scpRoles.Count)];
+                    }
+                    else
+                    {
+                        int r = plugin.Rng.Next(6);
+                        switch (r)
+                        {
+                            case 6:
+                                player.Role = RoleType.FacilityGuard;
+                                break;
+                            case 5:
+                            case 4:    
+                                player.Role = RoleType.Scientist;
+                                break;
+                            default:
+                                player.Role = RoleType.ClassD;
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Blackout/README.md
@@ -0,0 +1,22 @@
+Blackout Gamemode
+======
+## Description
+Most players will spawn normally, as Class-D, Scientists, Guards, etc. However, 1-5 SCP's will spawn of your choice. All the lights are out and everyone gets a free flashlight if they spawn!
+
+### Features
+Optional integration with the GamemodeManager.
+
+### Config Settings
+Config option | Config Type | Default Value | Description
+:---: | :---: | :---: | :------
+is_enabled | bool | true | Whether or not this gamemode is availible on the server.
+smart_scp_selection | bool | true | Whether or not the plugin should select the number of SCP's based on the server's current player count.
+max_scp_count | int | 5 | The maximum number of SCP's allowed to spawn at the start of the round.
+scp_to_spawn | RoleType | Scp173 | The SCP's that should spawn at the start of the round.
+
+### Commands
+Command | Optional Arguments | | Description
+:---: | :---: | :---: | :------
+**Aliases** | **blackout**
+(alias) enable | force, -f | ~~ | Enables the gamemode, starting on the next round start. If 'force' or '-f' is used, the gamemode will start instantly.
+(alias) disable | force, -f | ~~ | Disables the gamemode after the current round. If 'force' or '-f' is used, the gamemode will end immediately, and it will attempt to start a new, standard round as best it can. (Decon and nuke timer/status will not be reset.)
